db94bcedb30574b3d4336b2bcd277c9333e7aee2
Rohit Dhill <s4dr0t1@protonmail.com>
2022-08-12 02:07:01 +0530

    Enable hooking in the LSP to enable autocompletion

--- files/dot_config/nvim/lua/plugins/autopairs.lua
+++ files/dot_config/nvim/lua/plugins/autopairs.lua
@@ -1,8 +1,28 @@
 --[[
 	Name: autopairs.lua
 	Description: Pair autocompletion for [], {}, () and the likes
-	Link: https://github.com/windwp/nvim-autopairs
+	Link: windwp/nvim-autopairs
 --]]
 
 
-require('nvim-autopairs').setup {}
+--[[
+	Configuration
+--]]
+
+-- Making a protected call
+local status_ok, autopairs = pcall(require, "nvim-autopairs")
+if not status_ok then
+	print('There is something wrong with nvim-autopair')
+	return
+end
+
+-- Configuration
+autopairs.setup {}
+
+-- Hooking into the LSP to augment autocompletion
+local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+	return
+end
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
