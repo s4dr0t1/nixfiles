c8dd291905c9a7796752cd05705d338b9ef89dc0
Rohit Dhill <s4dr0t1@protonmail.com>
2022-08-12 18:11:41 +0530

    Critical! Making sure keymaps are passed to LSP
    Yay, diagnostic messages and other LSP related shortcuts work now, I was
    able to make it work by making sure the keybinds are passed to the LSP
    by defining my own custom on_attach event function named custom_attach and sending it over to the LSP
    at the on_attach event.
    
    Hopefully, I'll add more keybinds for the LSP in the future

--- files/dot_config/nvim/lua/lsp/server_attachment.lua
+++ files/dot_config/nvim/lua/lsp/server_attachment.lua
@@ -1,6 +1,7 @@
 --[[
 	Name: server_attachment.lua
 	Description: 
+		- This is the module which comes into picture when we need to attach a server to a buffer
 		- Making sure the LSP server gets attached to buffers
 		- Advertising capabilities of nvim-cmp to the LSP
 	Contains:
@@ -31,6 +32,9 @@ end
 mason_lspconfig.setup()
 
 --[[
+	Dynamic Server Startup
+	Startup servers installed via mason.nvim without having to manually specify every single one of them
+
 	Register a handler that will be called for all the servers, also make sure nvim-cmp adtertises to the that additional capabilities are supported
 	nvim-lsp-installer used the on_server_ready API which was deprecated a while back
 	What we need to try: https://github.com/LunarVim/Neovim-from-scratch/blob/0981b2838275468ad1863aba908379af63209e7a/lua/user/lsp/lsp-installer.lua#L6
@@ -40,7 +44,7 @@ mason_lspconfig.setup()
 -- the above is enough, but if you want to replicate the "on_server_ready" behaviour where your installed servers are setup "automatically" you can do the following
 
 
--- Protected call for cmp_nvim_lsp
+-- Protected call for cmp_nvim_lsp, required for capability advertisement to the LSP
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
 	print('There is something wrong with cmp_nvim_lsp')
@@ -51,11 +55,22 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
+local custom_attach = function(client, bufnr)
+	-- Passing the keymaps so that they can be used
+	lsp_keymaps(bufnr)
+
+	-- Disable formatting (for future reference)
+--	if client.name == "tsserver" then
+--		client.resolved_capabilities.document_formatting = false
+--	end
+end
+
 mason_lspconfig.setup_handlers {
 	function (server_name)
 		-- default handler - setup with default settings
 		lspconfig[server_name].setup {
 			capabilities = capabilities,
+			on_attach = custom_attach
 		}
 	end,
 
