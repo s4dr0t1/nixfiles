ab48c42f54bd6fd0d9a9f341ced9f1ce82bdbe8a
Rohit Dhill <s4dr0t1@protonmail.com>
2022-08-12 15:26:05 +0530

    Add proper settings for iamcco/markdown-preview

--- files/dot_config/nvim/lua/plugins/markdown.lua
+++ files/dot_config/nvim/lua/plugins/markdown.lua
@@ -1,8 +1,27 @@
 --[[
 	Name: markdown.lua
 	Description: Configuration file for the live-markdown server
-	Link: https://github.com/davidgranstrom/nvim-markdown-preview
+	Link: iamcco/markdown-preview.nvim
 --]]
 
+--[[
+	Configuration
+--]]
+
+
 local global = vim.g
-global.nvim_markdown_preview_format = 'markdown'
+
+-- Automatically start the server after opening a markdown file
+global.mkdp_auto_start = 1
+
+-- Shut down the server when we change the buffer
+global.mkdp_auto_close = 0
+
+-- Update the contents live
+global.mkdp_refresh_slow = 0
+
+-- Only bind to localhost
+global.mkdp_open_to_the_world = 0
+
+-- Default theme
+global.mkdp_theme = 'dark'
