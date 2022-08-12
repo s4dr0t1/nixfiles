50b00b6bc1b1938715a908cd5d5363ed487e421c
Rohit Dhill <s4dr0t1@protonmail.com>
2022-08-12 15:27:23 +0530

    Telescope: Add shortcuts and don't show binary files

--- files/dot_config/nvim/lua/plugins/telescope.lua
+++ files/dot_config/nvim/lua/plugins/telescope.lua
@@ -19,8 +19,53 @@ map('n', '<leader>fg', ':Telescope live_grep<cr>', { silent = true })
 map('n', '<leader>fb', ':Telescope buffers<cr>', { silent = true })
 map('n', '<leader>fh', ':Telescope help_tags<cr>', { silent = true })
 
+
+-- Some basic book-keeping that will be used in the configuration later
+local actions = require "telescope.actions"
+
+-- Don't preview binaries
+local previewers = require("telescope.previewers")
+local Job = require("plenary.job")
+local new_maker = function(filepath, bufnr, opts)
+	filepath = vim.fn.expand(filepath)
+	Job:new({
+		command = "file",
+		args = { "--mime-type", "-b", filepath },
+		on_exit = function(j)
+			local mime_type = vim.split(j:result()[1], "/")[1]
+			if mime_type == "text" then
+				previewers.buffer_previewer_maker(filepath, bufnr, opts)
+			else
+				-- maybe we want to write something to the buffer here
+				vim.schedule(function()
+					vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+				end)
+			end
+		end
+	}):sync()
+end
+
+
+
 -- Telescope settings
 telescope.setup {
+	defaults = {
+		-- Don't preview binaries
+		-- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#dont-preview-binaries
+		buffer_previewer_maker = new_maker;
+		mappings = {
+			i = {
+				["<C-n>"] = actions.cycle_history_next,
+				["<C-p>"] = actions.cycle_history_prev,
+
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+
+				["<C-c>"] = actions.close,
+				["<ESC>"] = actions.close,
+			}
+		}
+	},
 	extensions = {
 		fzf = {
 			fuzzy = true,
@@ -31,5 +76,5 @@ telescope.setup {
 	},
 }
 
--- To get fzf loaded and working with telescope, you need to call load_extension, somewhere after setup function:
+-- Loading extensions
 require('telescope').load_extension('fzf')
