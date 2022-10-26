cfd0cd96fa302855c9ceff500957945457059858
Rohit Dhill <s4dr0t1@protonmail.com>
2022-10-26 12:35:05 +0530

    Add Hyprland cachix

--- modules/desktop/wayland/hyprland/system.nix
+++ modules/desktop/wayland/hyprland/system.nix
@@ -8,4 +8,10 @@
 	programs = {
 		hyprland.enable = true;
 	};
+
+	# Hyprland cachix
+	nix.settings = {
+		substituters = ["https://hyprland.cachix.org"];
+		trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+	};
 }
